2e81b533a17633f30638e704503ff51cdb44761b
Daniel <ssingame@nate.com>
2016-02-28 23:19:46 +0900

    프로젝트 셋팅 / .gitignore 변경

---
+++ src/ch01/KC/HelloWorld.java
@@ -0,0 +1,12 @@
+package ch01.KC;
+
+/**
+ * Created by Daniel on 2016. 2. 28..
+ */
+public class HelloWorld {
+
+    public static void main(String args[]){
+        System.out.println("HelloWorld");
+    }
+
+}
