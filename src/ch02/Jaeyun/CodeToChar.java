1546713e82eb82d8b40848fa706e9d8d07ffcaf4
yoistudy <cosmicaeonj@gmail.com>
2016-03-13 10:45:26 +0900

    Ch02 예제 실습

---
+++ src/ch02/Jaeyun/CodeToChar.java
@@ -0,0 +1,14 @@
+package ch02.Jaeyun;
+
+public class CodeToChar {
+
+	public static void main(String[] args) {
+		int code = 65;
+		char ch = (char)code; // code에 저장된 값을 char형으로 변환하여 초기화 
+		
+		System.out.println(code);
+		System.out.println(ch);
+
+	}
+
+}
---
+++ src/ch02/Jaeyun/StringTest.java
@@ -0,0 +1,24 @@
+package ch02.Jaeyun;
+
+public class StringTest {
+
+	public static void main(String[] args) {
+		String a = 7 + " ";
+		String b = " " + 7;
+		String c = 7 + "";
+		String d = "" + 7;
+		String e = "" + "";
+		String f = 7 + 7 + "";
+		String g = "" + 7 + 7;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		
+	}
+
+}
