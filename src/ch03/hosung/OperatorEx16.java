639cdf0ff350bf14a8d36426fade7d3f88f02dd3
nahosung <skghtjd10@naver.com>
2016-03-18 18:09:37 +0900

    ch03-rm

--- src/ch03/hosung/OperatorEx16.java
+++
@@ -1,9 +0,0 @@
-package com.study.ch03;
-
-public class OperatorEx16 {
-	public static void main(String[] args) {
-		char lowerCase = 'a';
-		char upperCase = (char)(lowerCase - 32);
-		System.out.println(upperCase);
-	}
-}
--- src/ch03/hosung/OperatorEx21.java
+++
@@ -1,9 +0,0 @@
-package com.study.ch03;
-
-public class OperatorEx21 {
-	public static void main(String[] args) {
-		System.out.println(-10%8);
-		System.out.println(10%-8);
-		System.out.println(-10%-8);
-	}
-}
